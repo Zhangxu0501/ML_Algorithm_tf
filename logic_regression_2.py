d841f4b77b32867ae4915ab01d4fb84cf928fe16
81248397@qq.com <zhangxu@zhangxudeMacBook-Pro.local>
2016-11-04 16:40:26 +0800

    更新 logic_regression

--- logic_regression_2.py
+++ logic_regression_2.py
@@ -33,13 +33,7 @@ for i in range(0,200):
 # plt.scatter(x[100:],y[100:],s=20,c="blue")
 # plt.show()
 
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape=[])
-  return tf.Variable(initial)
 
-def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
 
 def get_max(arr):
     max=arr[0]
@@ -58,42 +52,43 @@ data=np.float32(data)
 labels=np.float32(labels)
 
 
+#数据准备结束,开始训练
+
 x_data=tf.placeholder(dtype=tf.float32,shape=[None,2])
 y_labels=tf.placeholder(dtype=tf.float32,shape=[None,2])
 
-# theta=weight_variable([2,2])
-# b = bias_variable([2])
+#正规化和偏差
+x_normal=tf.nn.l2_normalize(x_data,0)
 theta=tf.Variable(tf.random_normal(shape=[2,2]))
 b=tf.Variable(tf.random_normal(shape=[2]))
 
 
-h=tf.matmul(x_data,theta)+b
-h1=tf.nn.sigmoid_cross_entropy_with_logits(h, y_labels)
-cost = tf.reduce_mean(h1) # compute mean cross entropy (softmax is applied internally)
-train_op = tf.train.GradientDescentOptimizer(0.1).minimize(cost) # construct optimizer
+#z的计算
+h=tf.matmul(x_normal,theta)+b
+
+cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(h, y_labels)) # compute mean cross entropy (softmax is applied internally)
+train_op = tf.train.GradientDescentOptimizer(300.0).minimize(cost) # construct optimizer
 
 init=tf.initialize_all_variables()
 
 with tf.Session() as sess:
     sess.run(init)
-    print sess.run(h1,feed_dict={x_data:data,y_labels:labels})
+    for i in range(0,1000):
+        sess.run(train_op,feed_dict={x_data:data,y_labels:labels})
+        if i%200==0:
+            print sess.run(cost,feed_dict={x_data:data,y_labels:labels})
 
-    # for i in range(0,1000):
-    #     sess.run(train_op,feed_dict={x_data:data,y_labels:labels})
-    #     print sess.run(cost,feed_dict={x_data:data,y_labels:labels})
-    # end=sess.run(h,feed_dict={x_data:data,y_labels:labels})
-    #
-    # result=list()
-    #
-    # for i in end:
-    #     result.append(get_max(i))
-    #
-    # count=0
-    # for i in range(0,200):
-    #     if(result[i]==labels_r[i]):
-    #         count=count+1
-    # print "准确率为"+str(count/(len(result)+0.0))
-    # print count
+    get_max_labels=sess.run(h,feed_dict={x_data:data,y_labels:labels})
+    result=list()
+
+    for i in get_max_labels:
+        result.append(get_max(i))
+
+    count=0
+    for i in range(0,200):
+        if(result[i]==labels_r[i]):
+            count=count+1
+    print "准确率为"+str(count/(len(result)+0.0))
 
 
 
