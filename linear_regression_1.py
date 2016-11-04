de4a9f2029411cf8477307655dfff664230fcca9
81248397@qq.com <zhangxu@zhangxudeMacBook-Pro.local>
2016-11-04 11:51:20 +0800

    提交liner regression

--- linear_regression_1.py
+++ linear_regression_1.py
@@ -4,68 +4,46 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
-x=np.arange(0,6.283,0.01)
 
+x=np.arange(0,10,0.1)
 print x.shape
-y=np.sin(x)
-
-
-noise=np.random.random(629)*0.1
-noise_y=y+noise
-
-plt.plot(x,noise_y)
-
-x=x.reshape(629,1)
-x_train=np.ones(629)
-x_train=x_train.reshape(629,1)
-
-
-for i in range(1,10):
-    x_train=np.hstack((x_train,x**i))
-
-print x_train.shape
-
-
+y=x*0.3+3
 
 
 #开始训练
 
-xx=tf.constant(x_train,dtype=tf.float32,shape=[629,10])
-yy=tf.constant(noise_y,dtype=tf.float32,shape=[629,1])
+#place_holder
+xx=tf.constant(x,dtype=tf.float32,shape=[100,1])
+yy=tf.constant(y,dtype=tf.float32,shape=[100,1])
 
+#正规化
+xx_normal=tf.nn.l2_normalize(xx,0)
 
+#参数和偏差的定义
+theta=tf.Variable(tf.random_normal(shape=[1,1],mean=1))
+bais=tf.Variable(tf.random_normal([1,1]))
 
-
-xx=tf.nn.l2_normalize(xx,1)
-
-theta=tf.Variable(tf.random_uniform([10,1], -1.0, 1.0))
-bais=tf.Variable(tf.zeros([1]))
-
+#cost的计算
 result=tf.matmul(xx,theta)+bais
-
-
 cost=tf.reduce_mean(tf.square(yy-result))
 
-optimizer = tf.train
+#train_step的生成
+optimizer = tf.train.GradientDescentOptimizer(0.02)
 train_step= optimizer.minimize(cost)
-
-
 
 init=tf.initialize_all_variables()
 
 with tf.Session() as sess:
     sess.run(init)
-    print "========="
-    print sess.run(cost)
-    print "========="
-    for i in range(0,3000):
+    print "=================="
+    print "初始代价为:"+str(sess.run(cost))
+    print "=================="
+    print "迭代代价"
+    for i in range(0,1000):
         sess.run(train_step)
-        print sess.run(cost)
-        if i%1000==0:
+        if i%200==0:
             print sess.run(cost)
-    res=sess.run(result)
-    print res
-    print res.shape
-    print x.shape
-    plt.plot(x,res)
-    plt.show()
+    print "=================="
+    print "theta="+str(sess.run(theta))
+    print "bais="+str(sess.run(bais))
+    print "xx_normal="+str(sess.run(xx_normal))
